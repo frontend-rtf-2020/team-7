d1aa2770ec6ec8fa80efd5438e4aae1d0bc66e60
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-23 13:24:50 +0500

    mongo on heroku

--- data/db/index.js
+++ data/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const url = process.env.MONGODB_URI || 'mongodb+srv://Netwar:hWJQSaV909aGwZu3@cluster0-noog9.mongodb.net/Users?retryWrites=true&w=majority';
 
-const db = mongoose.connect('mongodb+srv://Netwar:hWJQSaV909aGwZu3@cluster0-noog9.mongodb.net/Users?retryWrites=true&w=majority', {
+const db = mongoose.connect(url, {
     useNewUrlParser: true,
 })
     .then (() => console.log('MongoDB connected'))
