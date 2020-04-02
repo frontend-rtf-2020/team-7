ed50ea3005becd87f80b11d349b5a42d5c6888b9
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-04-02 17:14:17 +0500

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://best-messenger.azurewebsites.net/api',
 });
 
 export const createUser = payload => api.post(`/registration`, payload);
