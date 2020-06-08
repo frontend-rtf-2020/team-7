1c263ac422dfff350ad913fb0e4dddc456963a74
LEXA-E6ATb <56407908+LEXA-E6ATb@users.noreply.github.com>
2020-06-09 00:11:46 +0500

    Update chat.js
    Исправлена сортировка вывода пользователей

--- server/events/chat.js
+++ server/events/chat.js
@@ -11,11 +11,10 @@ async function saveMessage(mes) {
 }
 
 async function chatList(user, socket) {
-  //обрабатываем информацию
-  const {fromUser} = user;
-  let users = await Chat.find({fromUser: fromUser}, 'toUser -_id');
-  let toCurrentUsers = await Chat.find({toUser: fromUser}, 'fromUser -_id');
-  let groupChat = await Chat.find({room: /[\w\s\d,]*/}, 'room -_id');
+  const { fromUser } = user;
+  let users = await Chat.find({ fromUser: fromUser }, 'toUser -_id');
+  let toCurrentUsers = await Chat.find({ toUser: fromUser }, 'fromUser -_id');
+  let groupChat = await Chat.find({ room: /[\w\s\d,]*/ }, 'room -_id');
   let list = [];
   for (let element of users) {
     let str = element.toString();
@@ -37,25 +36,36 @@ async function chatList(user, socket) {
   }
   for (let i = 0; i < list.length; i++) {
     let lastMessage;
-    if (list[i].split(", ").length === 1)
-      lastMessage = await Chat.find({
-        $or: [
-          {fromUser: fromUser, toUser: list[i]},
-          {fromUser: list[i], toUser: fromUser},
-        ],
-      }, 'fromUser message time -_id').sort({time: -1}).limit(1);
+    if (list[i].split(', ').length === 1)
+      lastMessage = await Chat.find(
+        {
+          $or: [
+            { fromUser: fromUser, toUser: list[i] },
+            { fromUser: list[i], toUser: fromUser },
+          ],
+        },
+        'fromUser message time -_id',
+      )
+        .sort({ time: -1 })
+        .limit(1);
     else
-      lastMessage = await Chat.find({room: list[i]}, 'fromUser message time -_id').sort({time: -1}).limit(1);
+      lastMessage = await Chat.find({ room: list[i] }, 'fromUser message time -_id')
+        .sort({ time: -1 })
+        .limit(1);
     lastMessage = lastMessage.toString().replace(/{[\w\s,:']*fromUser:\s*'/i, '\n\n');
     lastMessage = lastMessage.replace(/'[\w\s,:']*message:\s*'/i, ': ');
-    let date = new Date(lastMessage.split(/'[\s,]*[\s]*time:\s/i)[1].replace(/\s*}/i, '')).toString();
-    date = date.replace(/\w{3}\s\w{3}\s\d{2}\s\d{4}\s(\d{2}):(\d{2})[\w\s\d,:'+()]*/i, '$1:$2')
-    lastMessage = lastMessage.replace(/'[\s,]*[\s]*time:\s(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):\d{2}.\d{3}Z\s*}/i, '\n\n' + date);
+    let date = new Date(
+      lastMessage.split(/'[\s,]*[\s]*time:\s/i)[1].replace(/\s*}/i, ''),
+    ).toString();
+    date = date.replace(/\w{3}\s\w{3}\s\d{2}\s\d{4}\s(\d{2}):(\d{2})[\w\s\d,:'+()]*/i, '$1:$2');
+    lastMessage = lastMessage.replace(
+      /'[\s,]*[\s]*time:\s(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):\d{2}.\d{3}Z\s*}/i,
+      '\n\n' + date,
+    );
     if (lastMessage.includes(fromUser + ':'))
       lastMessage = lastMessage.replace(fromUser + ':', 'Вы:');
-    list[i] += lastMessage
+    list[i] += lastMessage;
   }
-  //отправляем вызываем на клиенте действие по событию 'returnChatList'
   return socket.emit('returnChatList', list);
 }
 
@@ -86,7 +96,10 @@ function parseStr(messages) {
     str = str.replace(/{[\w\s,:']*fromUser:\s*'/i, '');
     str = str.replace(/'[\w\s,:']*message:\s*'/i, '\n');
     let date = new Date(str.split(/'[\s,]*[\s]*time:\s/i)[1].replace(/\s*}/i, '')).toString();
-    str = str.replace(/'[\s,]*[\s]*time:\s(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):\d{2}.\d{3}Z\s*}/i, '\n' + date);
+    str = str.replace(
+      /'[\s,]*[\s]*time:\s(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):\d{2}.\d{3}Z\s*}/i,
+      '\n' + date,
+    );
     let dmy = str.split('\n')[2].replace(/\s\d{2}:\d{2}[\w\s\d,:'+()]*}/i, '');
     dmy = dmy.replace(/\w{3}\s(\w{3})\s(\d{2})\s(\d{4})\s\d{2}:\d{2}[\w\s\d,:'+()]*/i, '$2-$1-$3');
     if (!dmyArray.includes(dmy)) {
@@ -102,7 +115,7 @@ function parseStr(messages) {
 
 async function allUsers(user, socket) {
   const { username } = user;
-  let users = await User.find({}, 'username -_id').sort('username');
+  let users = await User.find({}, 'username -_id').collation({ locale: 'en' }).sort('username');
   let list = [];
   for (let element of users) {
     let str = element.toString();
@@ -113,7 +126,7 @@ async function allUsers(user, socket) {
   return socket.emit('returnAllUsers', list);
 }
 
-async function deleteDialog(user){
+async function deleteDialog(user) {
   const { fromUser, toUser } = user;
   await Chat.deleteMany({
     $or: [
@@ -124,11 +137,10 @@ async function deleteDialog(user){
 }
 
 async function updateGroupChat(data) {
-  const {toUser, room} = data;
+  const { toUser, room } = data;
   if (toUser !== room && room.trim() !== '')
-    await Chat.updateMany({room: toUser}, {room: room});
-  else
-    await Chat.deleteMany({room: toUser})
+    await Chat.updateMany({ room: toUser }, { room: room });
+  else await Chat.deleteMany({ room: toUser });
 }
 
 const chatHub = { saveMessage, chatList, messageList, allUsers, deleteDialog, updateGroupChat };
