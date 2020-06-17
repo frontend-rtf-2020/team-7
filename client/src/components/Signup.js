1d8bde1f976ea6d017e46914a34bdeda440a200a
Olga Korelina <56450175+pvpilizassal@users.noreply.github.com>
2020-06-17 21:18:48 +0500

    Update Signup.js
    Доработана система отправки сообщения и регистрации

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { sendemail, signup } from "../actions/session";
@@ -13,43 +13,73 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const Signup = ({ errors, signup, sendemail }) => {
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [code, setCode] = useState('');
+
+  const sendCode = (e) => {
     e.preventDefault();
     const user = {
-      email: e.target.email.value,
-      username: e.target.username.value,
-      password: e.target.password.value,
-      code: e.target.code.value,
+      email: email,
+      username: username,
+      password: password,
+    };
+    sendemail(user);
+  };
+
+  const signUp = (e) => {
+    e.preventDefault();
+    const user = {
+      email: email,
+      username: username,
+      password: password,
+      code: code,
     };
     signup(user);
-    sendemail(user);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleChangeCode = (e) => {
+    setCode(e.target.value);
   };
 
   return (
     <div className="box">
-      <form onSubmit={handleSubmit}>
+      <form>
         <span className="text-center">регистрация</span>
         <div className="input-container">
-          <input type="email" name="email" />
+          <input type="email" name="email" value={email} onChange={handleChangeEmail} />
           <label>Email:</label>
         </div>
         <div className="input-container">
-          <input type="text" name="username" />
+          <input type="text" name="username" value={username} onChange={handleChangeUsername} />
           <label>Username:</label>
         </div>
         <div className="input-container">
-          <input type="password" name="password" />
+          <input type="password" name="password" value={password} onChange={handleChangePassword} />
           <label>Password:</label>
         </div>
-        <input type="submit" value="Отправить код" />
+        <input type="submit" value="Отправить код" onClick={sendCode} />
         <span className="error-text">{errors}</span>
         <div className="input-container">
-          <input type="text" name="code" />
+          <input type="text" name="code" value={code} onChange={handleChangeCode} />
           <label>Проверочный код:</label>
         </div>
-        <input type="submit" value="Зарегистрироваться" />
+        <input type="submit" value="Зарегистрироваться" onClick={signUp} />
       </form>
-      <br></br>
+      <br/>
       <label className="label-text">
         Уже существует аккаунт?
         <Link to="/login">Авторизоваться</Link>
